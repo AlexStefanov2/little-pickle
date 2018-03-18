a06431b50306ba70c271b7003ba26bba41d55c85
Alex Stefanov <alex.stefanov2@gmail.com>
2018-03-18 08:12:18 +0200

    Put some styling into style.css

--- logout.js
+++ logout.js
@@ -1,10 +1,8 @@
 function logout() {
     var promise = Kinvey.User.logout()
         .then(function() {
-            window.alert('logout successful')
             window.location.replace(site);
         }).catch(function(error) {
-            window.alert('logout failed')
             window.alert(error);
         });
 }
