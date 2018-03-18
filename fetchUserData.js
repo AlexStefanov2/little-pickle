c14c0c102aa4eb36edef31644ffccc4714e6e12e
Alex Stefanov <alex.stefanov2@gmail.com>
2018-03-18 08:07:21 +0200

    Redo some of the previous commit

--- fetchUserData.js
+++ fetchUserData.js
@@ -1,13 +1,16 @@
+var promise;
+var publicvar;
+
 function fetchUserData(){
     document.getElementById('namecontainer').innerHTML = Kinvey.User.getActiveUser()['data']['username']
 
-            var query = new Kinvey.Query();
-            query.equalTo('username', Kinvey.User.getActiveUser()['data']['username']);
-            var stream = dataStore.find(query);
-            stream.subscribe(function onNext(entities){
-                if (entities.length > 0){
-                    publicvar = entities;
-                    document.getElementById('cookiecontainer').innerHTML = entities[0]['cookies'];
-                }
-            });
+    var query = new Kinvey.Query();
+    query.equalTo('username', Kinvey.User.getActiveUser()['data']['username']);
+    var stream = dataStore.find(query);
+    stream.subscribe(function onNext(entities){
+        if (entities.length > 0){
+            publicvar = entities;
+            document.getElementById('cookiecontainer').innerHTML = entities[0]['cookies'];
+        }
+    });
 }
